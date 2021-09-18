fe0d13449559d493daf48b8bc2c747ab04ec9b99
Eric C <39015447+ricetech@users.noreply.github.com>
2021-09-18 18:25:59 -0400

    Remove boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
+import HomePage from "./pages/home";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
