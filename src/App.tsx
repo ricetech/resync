0d017315cb1510c0946a00537db85e82bf9f38ea
Janakitti <janakittiratana@gmail.com>
2021-09-18 19:51:34 -0400

    Add AdminDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import "./styles/main.scss";
 import HomePage from "./pages/home";
+import StudentDashboard from "./pages/student-dashboard";
+import AdminDashboard from "./pages/admin-dashboard";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/student">
+          <StudentDashboard />
+        </Route>
+        <Route path="/admin">
+          <AdminDashboard />
+        </Route>
         <Route path="/">
           <HomePage />
         </Route>
---
+++ src/pages/admin-dashboard/AdminDashboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AdminDashboard = () => {
+  return (
+    <>
+      <h1>AdminDashboard</h1>
+    </>
+  );
+};
