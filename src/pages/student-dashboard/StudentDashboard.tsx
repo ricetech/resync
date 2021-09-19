e3dea6a1ec18db010dba96ef4adabbed10f72109
Janakitti <janakittiratana@gmail.com>
2021-09-18 21:05:16 -0400

    Add UpcomingList

--- src/pages/student-dashboard/StudentDashboard.tsx
+++ src/pages/student-dashboard/StudentDashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import DashboardNavbar from "../../components/dashboard-navbar";
 import CourseCard from "../../components/course-card";
+import UpcomingList from "../../components/upcoming-list";
 
 export const StudentDashboard = () => {
   return (
@@ -15,6 +16,7 @@ export const StudentDashboard = () => {
           </Col>
           <Col lg="4">
             <h1>Upcoming</h1>
+            <UpcomingList />
           </Col>
         </Row>
       </Container>
