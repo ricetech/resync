89d1c95bea902ccd5cc0534622a829ee1480b970
Janakitti <janakittiratana@gmail.com>
2021-09-18 20:19:15 -0400

    Begin layout for StudentDashboard

--- src/pages/student-dashboard/StudentDashboard.tsx
+++ src/pages/student-dashboard/StudentDashboard.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import DashboardNavbar from "../../components/dashboard-navbar";
 
 export const StudentDashboard = () => {
   return (
     <>
       <DashboardNavbar />
-      <h1>StudentDashboard</h1>
+      <Container>
+        <Row className="py-5">
+          <Col lg="8">
+            <h1>Courses</h1>
+          </Col>
+          <Col lg="4">
+            <h1>Upcoming</h1>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
